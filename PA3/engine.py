6f5312e3825c86698e3365192419a8a7b727e475
hblyx <yal064@ucsd.edu>
2022-02-19 17:55:26 -0800

    Revert "Tom - c"
    This reverts commit 3de503e73f1f71d15664d2926d58956348212aac.

--- PA3/engine.py
+++ PA3/engine.py
@@ -1,6 +1,7 @@
 # Modified by Colin Wang, Weitang Liu
 import torch
 import numpy as np
+import torchvision
 
 from data import *
 from model import baseline, custom1
@@ -31,6 +32,12 @@ def create_model(device, model_type="baseline"):
         model = baseline().to(device)
     elif model_type == "custom":  # had used to build experiments
         model = custom1().to(device)
+    elif model_type == "resnet18":
+        model = torchvision.models.resnet18(pretrained=True).to(device)
+        model.fc = torch.nn.Linear(in_features=512, out_features=20)
+    elif model_type == "vgg16":
+        model = torchvision.models.vgg16_bn(pretrained=True).to(device)
+        model.classifier[6] = torch.nn.Linear(in_features=4096, out_features=20, bias=True)
     else:
         model = baseline().to(device)
 
@@ -92,6 +99,63 @@ def train_model(model, criterion, optimizer, device, dataloaders, max_epoch=25, 
     # return the model with weight selected by best performance
     return model, train_loss, val_loss, train_acc, val_acc
 
+def train_model_resnet(model, criterion, optimizer, device, dataloaders, scheduler, max_epoch=25, model_type="resnet_18"):
+    model.train()
+
+    cp_name = model_type + ".pt"
+
+    train_loss, val_loss = [], []
+    train_acc, val_acc = [], []
+    best_val_loss = float('inf')
+
+    for epoch in range(max_epoch):
+        train_l = []
+        correct_train, total_train = 0, 0
+
+        print("Epoch", epoch, "starts")
+        print("Going through training set")
+        for batch_idx, (X, y) in enumerate(dataloaders[0]):  # training set
+            # move to GPU
+            X, y = X.to(device), y.to(device)
+            optimizer.zero_grad()  # zero gradients
+            output = model(X)  # forward pass
+            loss = criterion(output, y)  # compute loss
+            loss.backward()
+            optimizer.step()
+
+            train_l.append(loss.item())
+
+            # accuracy
+            preds = torch.argmax(output.data, 1)
+            total_train += y.shape[0]
+            correct_train += torch.sum(preds == y)
+
+        train_loss.append(np.mean(train_l))
+        train_acc.append(float(correct_train) / float(total_train))
+
+        print("Going through validation set")
+        val_los, val_ac = evaluate(dataloaders[1], model, criterion, device)
+
+        val_loss.append(val_los)
+        val_acc.append(val_ac)
+
+        if val_los < best_val_loss:  # if current validation loss is less
+            best_val_loss = val_los
+            torch.save(model.state_dict(), cp_name)
+
+        if scheduler is not None:
+            scheduler.step()
+
+        print("Epoch", epoch, "ends")
+
+    model = torchvision.models.resnet18(pretrained=True)
+    model.fc = torch.nn.Linear(in_features=512, out_features=20)
+    model.to(device)
+    model.load_state_dict(torch.load(cp_name))
+
+    # return the model with weight selected by best performance
+    return model, train_loss, val_loss, train_acc, val_acc
+
 
 # add your own functions if necessary
 def evaluate(dataloader, model, criterion, device, is_test=False):
@@ -115,7 +179,7 @@ def evaluate(dataloader, model, criterion, device, is_test=False):
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     acc = correct / len(dataloader.dataset)
-    loss = np.sum(loss)
+    loss = np.mean(loss)
 
     print('\n{} set: Accuracy: {}/{} ({:.0f}%)\n'.format(
         test, correct, len(dataloader.dataset),
